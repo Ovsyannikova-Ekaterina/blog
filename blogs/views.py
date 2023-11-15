6525b72838c300c71cfd064cbfa434889c36d049
Kate <xilatomil@gmail.com>
2023-11-16 00:22:22 +0300

    edit_post

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-# from django.http import Http404
+from django.http import Http404
 from .models import BlogPost
 from .forms import BlogPostForm
 
@@ -21,17 +21,17 @@ def post_details(request, post_id):
 def edit_post(request, post_id):
     # Получаем конкретный блог-пост по его идентификатору
     post = BlogPost.objects.get(id=post_id)
-
+    if post.author != request.user:
+        raise Http404
     if request.user == post.author:
         if request.method == 'POST':
             # Если данные отправлены методом POST, создаем форму на основе полученных данных
             form = BlogPostForm(request.POST, instance=post)
             if form.is_valid():
                 form.instance.author = request.user  # Установите автора перед сохранением
-                # Если форма прошла валидацию, сохраняем изменения в блог-посте
-                updated_post = form.save()  # Сохраняем изменения и получаем обновленный пост
-                return redirect('post_details', post_id=updated_post.id)  # Перенаправляем на страницу post_details с
-                # обновленным постом
+                form.save()
+                # updated_post = form.save()  # Сохраняем изменения и получаем обновленный пост
+                return redirect('blogs:post_details', post_id=post.id)
         else:
             # Если данные не отправлены методом POST, показываем форму для редактирования
             form = BlogPostForm(instance=post)
@@ -43,23 +43,20 @@ def edit_post(request, post_id):
 
 @login_required
 def new_post(request):
-    if request.method == 'POST':
-        # Если данные отправлены методом POST, создаем форму на основе полученных данных
+    if request.method != 'POST':
+        form = BlogPostForm()
+    else:
         form = BlogPostForm(request.POST)
         if form.is_valid():
-            # Если форма прошла валидацию, сохраняем новый блог-пост
-            # blog_post = form.save(commit=False)
-            # blog_post.author = request.user  # Устанавливаем текущего пользователя как автора поста
-            # blog_post.save()
-            form.instance.author = request.user  # Установите автора перед сохранением
-            form.save()
+            blog_post = form.save(commit=False)
+            blog_post.author = request.user  # Устанавливаем текущего пользователя как автора поста
+            blog_post.save()
+            # form.instance.author = request.user  # Установите автора перед сохранением
+            # form.save()
             return redirect('blogs:home')
-    else:
-        # Если данные не отправлены методом POST, показываем пустую форму
-        form = BlogPostForm()
     return render(request, 'blogs/new_post.html', {'form': form})
 
-
+@login_required
 def my_posts(request):
     user_posts = BlogPost.objects.filter(author=request.user)
     return render(request, 'blogs/my_posts.html', {'user_posts': user_posts})
